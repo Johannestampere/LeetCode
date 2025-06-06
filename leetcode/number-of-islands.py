79ddc3912abd113bd24f5fd4bdbb1da02cfaf172
Johannes Tampere <80104176+Johannestampere@users.noreply.github.com>
2025-06-05 22:02:27 -0400

    Update solution for number-of-islands

--- leetcode/number-of-islands.py
+++ leetcode/number-of-islands.py
@@ -1,39 +1,30 @@
-class Solution {
-public:
+# O(m x n)
 
-    int m, n;
-    vector<vector<char>>* gridPtr;
+class Solution(object):
+    def numIslands(self, grid):
+        m, n = len(grid), len(grid[0])
 
-    void dfs(int i, int j) {
-            if (i < 0 || i >= m || j < 0 || j >= n || (*gridPtr)[i][j] != '1') return;
-
-            (*gridPtr)[i][j] = '0';
-
-            dfs(i, j + 1);
-            dfs(i + 1, j);
-            dfs(i, j - 1);
-            dfs(i - 1, j);
-        }
-
-    int numIslands(vector<vector<char>>& grid) {
+        # marks entire visited land as water (0-s)
+        def dfs(i, j):
+            # check out of bounds or pos is not land
+            if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] != "1":
+                return
+            else:
+                grid[i][j] = "0"
+                # do the same for up, down, left, right to delete the island
+                dfs(i, j + 1)
+                dfs(i + 1, j)
+                dfs(i, j - 1)
+                dfs(i - 1, j)
                 
-        m = grid.size();
-        n = grid[0].size();
-        gridPtr = &grid;
-        int count = 0;
 
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == '1') {
-                    dfs(i, j);
-                    ++count;
-                }
-            }
-        }
+        n_islands = 0
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    n_islands += 1
+                    # cant double count
+                    dfs(i, j)
 
-        return count;
-
-    }
-
-    
-};
+        return n_islands
