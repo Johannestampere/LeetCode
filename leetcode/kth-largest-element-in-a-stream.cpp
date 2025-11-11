6408965df872f379cf6339abd57db5ce8f73c123
Johannes Tampere <80104176+Johannestampere@users.noreply.github.com>
2025-11-11 21:13:10 +0200

    Add solution for kth-largest-element-in-a-stream

---
+++ leetcode/kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,26 @@
+#include <queue>
+
+class KthLargest {
+    int num;
+    priority_queue<int, vector<int>, greater<int>> pq;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        num = k;
+        for (auto el : nums) {
+            pq.push(el);
+            if (pq.size() > k) { pq.pop(); }
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if (pq.size() > num) { pq.pop(); }
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
