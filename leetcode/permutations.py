770fd9ad49198199ab7a4a084044dd4f1ab2f4ed
Johannes Tampere <80104176+Johannestampere@users.noreply.github.com>
2025-11-17 12:11:30 +0200

    Update solution for permutations

--- leetcode/permutations.py
+++ leetcode/permutations.py
@@ -1,15 +1,21 @@
-class Solution(object):
-    def permute(self, nums):
-        # n! * (n^2)
-        if len(nums) == 0:
-            return [[]]
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        backtrack(nums, 0, res);
+        return res;
+    }
+private:
+    void backtrack(vector<int>& nums, int start, vector<vector<int>>& res) {
+        if (start == static_cast<int>(nums.size())) {
+            res.push_back(nums);
+            return;
+        }
 
-        perms = self.permute(nums[1:]) # take the first num from nums out and add it to every index in every possible permutation of all the others 
-        res = []
-        for p in perms:
-            for i in range(len(p) + 1):
-                cpy = list(p)
-                cpy.insert(i, nums[0])
-                res.append(cpy)
-
-        return res
+        for (int i = start; i < nums.size(); ++i) {
+            swap(nums[start], nums[i]);
+            backtrack(nums, start+1, res);
+            swap(nums[start], nums[i]);
+        }
+    }
+};
