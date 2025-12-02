7e0ad38872660b0a81d0d026a05c33a4d30e2421
Johannes Tampere <80104176+Johannestampere@users.noreply.github.com>
2025-12-02 22:27:35 +0200

    Add solution for house-robber

---
+++ leetcode/house-robber.py
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int rob1 = 0;
+        int rob2 = 0;
+
+        for (int n : nums) {
+            int tmp = max(n + rob1, rob2);
+            rob1 = rob2;
+            rob2 = tmp;
+        }
+
+        return rob2;
+    }
+};
