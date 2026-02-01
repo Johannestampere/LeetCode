8272ad99a1575160ef4934dd45c97a54b00a9581
Johannes Tampere <80104176+Johannestampere@users.noreply.github.com>
2026-01-31 20:02:14 -0500

    Add solution for remove-duplicates-from-sorted-array

---
+++ leetcode/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        i,j=1,1
+
+        while i < len(nums):
+            if nums[i] != nums[i-1]:
+                nums[j] = nums[i]
+                j+=1
+            i += 1
+
+        return j
